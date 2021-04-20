4b342955b134538e1406186e37896eb20842e9d3
V <vin.nussbaumer@gmail.com>
2021-04-20 15:46:29 +0200

    Gravity modulation changes
    Physics2DServer gravity now used to emulate gravity and modulate it, keybinds Q,W,E added, modification of the initial player spawn.

--- Player.gd
+++ Player.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 
-const ACCELERATION = 513
+
+const ACCELERATION = 512
 const JUMP_IMPULSE = 100
-const GRAVITY = 200
+
 const AIR_FRICTION = 0.03
 const GROUND_FRICTION = 0.4
 const MAX_SPEED = 64
 
 var velocity = Vector2.ZERO
+var circle_shape
 
 onready var sprite = $Sprite
 onready var animationplayer = $AnimationPlayer
@@ -15,10 +17,14 @@ onready var animationplayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(200,0))
+	Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY, 200)
 	pass # Replace with function body.
 
 func _physics_process(delta):
 	# Player inputs
+	
+	var GRAVITY = 200;
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 
 	if x_input != 0:
@@ -48,9 +54,19 @@ func _physics_process(delta):
 	if x_input == 0:
 		velocity.x = lerp(velocity.x, 0, friction)
 	
+	GRAVITY = Physics2DServer.area_get_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY)
 	velocity.y += GRAVITY * delta
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	_grav_con()
+
+func _grav_con():
+	if Input.is_action_just_pressed("ui_q"):
+		Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY, 400)
+	if Input.is_action_just_pressed("ui_w"):
+		Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY, 200)
+	if Input.is_action_just_pressed("ui_e"):
+		Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY, 100)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
