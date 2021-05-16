f9d08fab4a555a73acafbe5ea7c8bb2405d42585
ddorn <diego.dorn@free.fr>
2021-05-16 19:18:08 +0200

    add simple button

---
+++ Button.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var pressed = 0
+
+signal pressed
+signal unpressed
+
+func _on_Button_body_entered(body):
+	if pressed == 0:
+		$Click.play()
+		emit_signal("pressed")
+
+	pressed += 1
+
+func _on_Button_body_exited(body):
+	pressed -= 1
+	
+	if pressed == 0:
+		$Unclick.play()
+		emit_signal("unpressed")
