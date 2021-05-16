40f6476e6f5f5856e0c9b5a7b7ad9e4e78cf78e4
ddorn <diego.dorn@free.fr>
2021-05-16 18:10:00 +0200

    add torch + start level2

---
+++ Torch.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+const Utils = preload("res://utils.gd")
+
+func _ready():
+	$Animation.play("default")
+	$Animation.seek(randf() * $Animation.current_animation_length)
+
+func _process(delta):
+	var gravity = Utils.get_gravity(self)
+	
+	var prev_state = $Animation.current_animation
+
+	if gravity > 300:
+		$Animation.current_animation = "high_grav"
+	else:
+		$Animation.current_animation = "default"
+
+	if prev_state != $Animation.current_animation:
+		# Randomize start on change
+		$Animation.seek(randf() * $Animation.current_animation_length)
