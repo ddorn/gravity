4e5ae28355fea2c03fba7dcce6dfdde84717a56f
ddorn <diego.dorn@free.fr>
2021-05-18 15:02:52 +0200

    cannot stand on spikes on hig grav

--- Spike.gd
+++ Spike.gd
@@ -4,12 +4,10 @@ const Utils = preload("res://utils.gd")
 
 func collide_with(collision: KinematicCollision2D, body):
 	if body.has_method("kill"):
-
-		
 		# 200 is the default gravity. So by default RHS is 1. 
 		var normalized_repulsion = 200.0 / Utils.get_gravity(self)
 		# By default, if the player is faster than 20 px/s they die.
-		var normalized_velocity = body.last_velocity.y / 20.0
+		var normalized_velocity = (body.last_velocity.y / 2 + 15) / 20.0
 		
 		if normalized_velocity > normalized_repulsion:
 			body.kill()
