d192275c24311e6bebf1dfc5964fd44911353af1
ddorn <diego.dorn@free.fr>
2021-05-23 10:05:24 +0200

    add transparency to the dialogue + few cleanups

--- Dialogue/Dialogue.gd
+++ Dialogue/Dialogue.gd
@@ -1,16 +1,9 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	hide();
 	if PlayerVariables.scene == 2 && PlayerVariables.mort == 1 :
 		show();
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
--- Level1.gd
+++ Level1.gd
@@ -1,19 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if !(PlayerVariables.scene == 2):
 		PlayerVariables.mort = 0;
 		PlayerVariables.scene = 2;
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
