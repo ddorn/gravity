68ff52ca7ff4d1db9793b499c581d8eaaf9912d8
ddorn <diego.dorn@free.fr>
2021-05-24 16:00:41 +0200

    hint for spikes

--- Level1.gd
+++ Level1.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+const Utils = preload("res://utils.gd")
 
 func _ready():
 	if !(PlayerVariables.scene == 2):
 		PlayerVariables.mort = 0;
 		PlayerVariables.scene = 2;
+
+
+func _process(delta):
+	if $SpikeHint.overlaps_body($Player):
+		if Utils.get_gravity(self) < 200:
+			$SpikeHint/Hint.trigger()
