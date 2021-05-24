5f43ae8b2edf69ad98fcd3df9c33750dbfcacd06
ddorn <diego.dorn@free.fr>
2021-05-24 16:53:48 +0200

    dont die above sreen

--- Player.gd
+++ Player.gd
@@ -98,7 +98,7 @@ func _physics_process(delta):
 			collision.collider.push(velocity, snap, self)
 			
 	# Kill out of screen
-	if not (-10 < position.x and position.x < 360 + 10 and -10 < position.y and position.y < 180 + 10):
+	if not (-10 < position.x and position.x < 360 + 10 and -10 < position.y):
 		kill()
 
 
